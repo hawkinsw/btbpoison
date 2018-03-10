5a0c3061d936188d456753f70282d169a117a55c
Will Hawkins <whh8b@virginia.edu>
2018-03-10 01:17:00 -0500

    Add support for MSR monitoring in poison.c

--- Makefile
+++ Makefile
@@ -1,31 +1,49 @@
-all: poison pipe poison.obj time_mispredict_asm spy victim time_mispredict_c
-
+all: poison pipe poison.obj time_mispredict_asm spy victim time_mispredict_c spy.obj victim.obj time_jump
 
 EXTRA_CFLAGS=-std=gnu99 -O0
+EXTRA_CXXFLAGS=-std=c++11 -O0
 ifdef ASLR
 EXTRA_CFLAGS+= -fPIC -fPIE
+EXTRA_CXXFLAGS+= -fPIC -fPIE
 endif
 ifdef DEBUG
 EXTRA_CFLAGS+= -g
+EXTRA_CXXFLAGS+= -g
 endif
 
 spy: spyvictim.c Makefile
+	gcc -o spy.s -S spyvictim.c -DSPY $(EXTRA_CFLAGS)
 	gcc -o spy spyvictim.c -DSPY $(EXTRA_CFLAGS)
 victim: spyvictim.c Makefile
 	gcc -o victim spyvictim.c -DVICTIM $(EXTRA_CFLAGS)
+spy.obj: spy
+	objdump -D spy > spy.obj
+victim.obj: victim
+	objdump -D victim > victim.obj
+
+time_jump: time_jump.c Makefile
+	gcc -o time_jump time_jump.c $(EXTRA_CFLAGS)
+
 poison: poison.c Makefile
+ifndef MSR
 	gcc -o poison poison.c $(EXTRA_CFLAGS)
 	gcc -S -o poison.s poison.c $(EXTRA_CFLAGS)
-pipe: pipe.c Makefile
-	gcc -o pipe pipe.c $(EXTRA_CFLAGS)
+else
+	g++ -o poison poison.c -DUSEMSR -I../libmsr/ -L../libmsr/ -lmsr $(EXTRA_CXXFLAGS)
+endif
 poison.obj: poison
 	objdump -D poison > poison.obj
+
+pipe: pipe.c Makefile
+	gcc -o pipe pipe.c $(EXTRA_CFLAGS)
+
 time_mispredict_asm: time_mispredict_asm.s
 	nasm -felf64 time_mispredict_asm.s -o time_mispredict_asm.o
 	gcc time_mispredict_asm.o -o time_mispredict_asm
 time_mispredict_c: time_mispredict_c.c Makefile
 	gcc -o time_mispredict_c time_mispredict_c.c $(EXTRA_CFLAGS)
+
 clean: 
-	rm -f poison *.o core pipe spy victim time_mispredict_asm time_mispredict_c poison.s
+	rm -f poison *.o core pipe spy victim time_mispredict_asm time_mispredict_c poison.s victim.obj spy.obj time_jump
 clean_pipes:
 	rm -f /tmp/parent_to_child.pipe /tmp/child_to_parent.pipe
--- poison.c
+++ poison.c
@@ -10,6 +10,14 @@
 #include <string.h>
 #include <stdbool.h>
 
+#ifdef USEMSR
+#include <assert.h>
+#include <iostream>
+#include "msr.hpp"
+#endif
+
+#define CPU_NO 5
+
 extern int errno;
 
 static inline __attribute__((always_inline)) unsigned long long rdtsc(void)
@@ -19,12 +27,30 @@ static inline __attribute__((always_inline)) unsigned long long rdtsc(void)
 	return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 }
 
-unsigned long long cross(int target, int loop_max) {
+unsigned long long cross(int target,
+	int loop_max,
+	const char *context
+#ifdef USEMSR
+	,
+	unsigned long long *misses
+#endif
+	) {
 	register void *t asm ("r12") = NULL;
-	int value = 0;
-	volatile float result = 0.0;
 	unsigned long long before, after;
 	unsigned long long delta = 0;
+
+#ifdef USEMSR
+	Msr msr;
+	uint64_t sel0 = 0x0;
+	uint64_t sel1 = 0x0;
+	uint64_t perf_ctr = 0x0;
+	uint64_t msr_result = 0x0;
+	uint64_t zero = 0x0;
+
+	*misses = 0;
+#endif
+
+
 
 	switch (target) {
 		case 1:
@@ -37,6 +63,28 @@ unsigned long long cross(int target, int loop_max) {
 
 	for (int i = 0; i<loop_max; i++)
 	{
+
+#ifdef USEMSR
+	perf_ctr = MSR::PERF_EVTSEL0_ENABLE | MSR::PERF_EVTSEL1_ENABLE;
+
+	sel0 = MSR::EVT_BR_MISP_RETIRED |
+	       MSR::MASK_BR_MISP_RETIRED_ALL |
+	       MSR::EVENT_ENABLE |
+	       MSR::EVENT_USER_ENABLE;
+
+	sel1 = MSR::EVT_BR_INST_RETIRED |
+	       MSR::MASK_BR_INST_RETIRED_ALL |
+	       MSR::EVENT_ENABLE |
+	       MSR::EVENT_USER_ENABLE;
+
+	assert(msr.Write(MSR::MSR_CORE_PERF_GLOBAL_CTRL, zero, CPU_NO) == 0);
+	assert(msr.Write(MSR::MSR_IA32_PERFEVTSEL0, sel0, CPU_NO) == 0);
+	assert(msr.Write(MSR::MSR_IA32_PERFEVTSEL1, sel1, CPU_NO) == 0);
+	assert(msr.Write(MSR::MSR_IA32_PMC0, zero, CPU_NO) == 0);
+	assert(msr.Write(MSR::MSR_IA32_PMC1, zero, CPU_NO) == 0);
+	(msr.Write(MSR::MSR_CORE_PERF_GLOBAL_CTRL, perf_ctr, CPU_NO) == 0);
+#endif
+
 		before = rdtsc();
 		asm goto("jmp *%%r12\n" : /*output*/:/*input*/:/*clobbers*/:  T1, T2);
 		asm ("nop; nop; nop; nop;\n");
@@ -52,10 +100,30 @@ unsigned long long cross(int target, int loop_max) {
 		asm ("nop; nop; nop; nop;\n");
 T1:
 		after = rdtsc();
-		delta = (after - before);
+#ifdef USEMSR
+	(msr.Write(MSR::MSR_CORE_PERF_GLOBAL_CTRL, zero, CPU_NO) == 0);
+	assert(msr.Write(MSR::MSR_IA32_PERFEVTSEL0, zero, CPU_NO) == 0);
+
+	assert(msr.Read(MSR::MSR_IA32_PMC1, msr_result, CPU_NO) == 0);
+	//std::cout << context << " branch count: " << std::dec << msr_result << std::endl;
+	assert(msr.Read(MSR::MSR_IA32_PMC0, msr_result, CPU_NO) == 0);
+	//std::cout << context << " missed branch count: " << std::dec << msr_result << std::endl;
+	*misses = msr_result;
+#endif
+	delta = (after - before);
 		continue;
 T2:
 		after = rdtsc();
+#ifdef USEMSR
+	assert(msr.Write(MSR::MSR_CORE_PERF_GLOBAL_CTRL, zero, CPU_NO) == 0);
+	assert(msr.Write(MSR::MSR_IA32_PERFEVTSEL0, zero, CPU_NO) == 0);
+
+	assert(msr.Read(MSR::MSR_IA32_PMC1, msr_result, CPU_NO) == 0);
+	//std::cout << context << " branch count: " << std::dec << msr_result << std::endl;
+	assert(msr.Read(MSR::MSR_IA32_PMC0, msr_result, CPU_NO) == 0);
+	//std::cout << context << " missed branch count: " << std::dec << msr_result << std::endl;
+	*misses = msr_result;
+#endif
 		delta = (after - before);
 		continue;
 	}
@@ -76,13 +144,14 @@ int main() {
 	unsigned int iterations = 0;
 	unsigned int inner_iterations = 10;
 	unsigned int counter = 1;
+	unsigned long long misses = 0;
 
 	float average = 0.0;
 
 	cpu_set_t cpu_mask;
 
 	CPU_ZERO(&cpu_mask);
-	CPU_SET(0, &cpu_mask);
+	CPU_SET(CPU_NO, &cpu_mask);
 	sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);
 
 	if (pipe(child_to_parent_pipe) || pipe(parent_to_child_pipe)) {
@@ -90,17 +159,19 @@ int main() {
 		return 1;
 	}
 
-	totals = cross(1,1);
+#if 0
+	totals = cross(1,1, "None");
 	printf("uncached time jump to 1: %llu\n", totals);
-	cross(1,1000);
-	totals = cross(1,1);
+	cross(1,1000, "None");
+	totals = cross(1,1, "None");
 	printf("  cached time jump to 1: %llu\n", totals);
 
-	totals = cross(2,1);
+	totals = cross(2,1, "None");
 	printf("uncached time jump to 2: %llu\n", totals);
-	cross(2,1000);
-	totals = cross(2,1);
+	cross(2,1000, "None");
+	totals = cross(2,1, "None");
 	printf("  cached time jump to 2: %llu\n", totals);
+#endif
 
 	totals = 0.0;
 
@@ -110,36 +181,52 @@ int main() {
 	p2c_read = parent_to_child_pipe[0];
 	p2c_write= parent_to_child_pipe[1];
 
+
 	if (!(child = fork())) {
 		for (int i = 0; i<counter; i++) {
 			read(p2c_read, &just_read, 1);
 
-			totals += cross(1, 25);
-
+#ifndef USEMSR
+			totals += cross(2, 100, "Child");
+#else
+			totals += cross(2, 100, "Child", &misses);
+#endif
 			write(c2p_write, &to_write, 1);
 
 			iterations += 1;
 		}
+#ifndef USEMSR
 		average = (totals*1.0) / iterations;
 		printf("child time per iteration: %f\n", average);
+#else
+		average = (misses*1.0) / (iterations);
+		printf("child misses per iteration: %f\n", average);
+#endif
 		return 0;
 	}
 
 	for (int i = 0; i<counter; i++) {
 		/*
 		 * This is optional.
-		fake = cross(1, 25);
+		fake = cross(2, 25);
 		 */
 		write(p2c_write, &to_write, 1);
 		read(c2p_read, &just_read, 1);
-		totals += cross(2, 1);
-
+#ifndef USEMSR
+		totals += cross(1, 1, "Parent");
+#else
+		totals += cross(1, 1, "Parent", &misses);
+#endif
 		iterations+=1;
 	}
 
+#ifndef USEMSR
 	average = (totals*1.0) / iterations;
 	printf("parent time per iteration: %f\n", average);
-
+#else
+	average = (misses*1.0) / iterations;
+	printf("parent misses per iteration: %f\n", average);
+#endif
 
 	waitpid(child, NULL, 0);
 
